05f4748338a3ebe7b88654843326ddebcb517abf
Razmig KECHICHIAN <razmig.kechichian@creatis.insa-lyon.fr>
2024-12-15 14:26:43 +0100

    minor code cleanups

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # %% IMPORTS
 
 import time
@@ -12,11 +13,13 @@ from torchvision import datasets
 from torchvision.transforms import Compose, ToTensor, Normalize, \
                                    RandomRotation, InterpolationMode
 from torch.utils.tensorboard import SummaryWriter
+from torchinfo import summary
 
 import network
 from train_val import train_loop, validation_loop, update_graphs
 
-# %% PARAMETERS : should be able to provide as arguments to the module
+
+# %% HYPER-PARAMETERS : should also be able to provide as arguments to the module
 
 epochs = 10
 learning_rate = 1e-3
@@ -25,12 +28,25 @@ mbatch_size = 32
 mbatch_group = -1
 num_workers = 8
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# %% TARGET DEVICE
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+print(f"Using {device} device.")
+
 
 # %% DATA LOADING
 
 test_transform = Compose([ToTensor(),
                           Normalize((0.1307,), (0.3081,))])
+                          
 train_transform = Compose([RandomRotation([-20, 20],
                            InterpolationMode.BILINEAR),
                            test_transform])
@@ -48,23 +64,31 @@ test_loader = torch.utils.data.DataLoader(test_set, batch_size=mbatch_size,
                                          
 num_classes = len(train_set.classes)
 
-# %% MODEL CREATION
+
+# %% MODEL CREATION AND SUMMARY
 
 net = network.Net()
 net = net.to(device)
+summary(net, input_size=(1, 1, 28, 28), col_names=["input_size", "output_size", "num_params"])
+
+
+# %% LOSS FUNCTION AND OPTIMIZER
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=learning_rate,
                        weight_decay=weight_decay)
+
 
 # %% TENSORBOARD WRITER INSTANTIATION
 
 timestamp = datetime.datetime.now().strftime('%d-%m-%Y_%H:%M:%S')
 summary_writer = SummaryWriter("./runs/mnist_sandbox_{}".format(timestamp))
 
+
 # %% TRAIN/VAL LOOP
 
 training_time = 0
+
 for t in range(epochs):
     print(f"EPOCH {t+1:4d}", 70*"-", flush=True)
 
@@ -73,8 +97,7 @@ for t in range(epochs):
     toc = time.time()
     training_time += (toc - tic)
     train_res = validation_loop(train_loader, net, criterion, num_classes, device)
-    test_res = validation_loop(test_loader, net, criterion, num_classes, device)
-    
+    test_res = validation_loop(test_loader, net, criterion, num_classes, device)    
     update_graphs(summary_writer, t, train_res, test_res)
 
 summary_writer.close()
